cd1fe3dd02368fa94aff62cfd9bb428e4c28f76f
SGauthier2Pro <Sylvain.Gauthier2Pro@gmail.com>
2023-07-06 17:46:45 +0200

    test changes to start a build push

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.views.static import serve
+from django.conf import settings
 
 from .views.index import index
 
@@ -8,4 +11,6 @@ urlpatterns = [
     path(r'lettings/', include('lettings.urls', namespace='lettings')),
     path(r'profiles/', include('profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
